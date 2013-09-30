243232a5b5b76fb73b9956c5af326a12184d53ea
alex <itmedved@gmail.com>
2013-09-30 10:42:50 +0300

    chenge event on clik

--- www/js/index.js
+++ www/js/index.js
@@ -1,26 +1,7 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
-        var scaner = new BarcodeScanner();
     },
     // Bind Event Listeners
     //
@@ -31,28 +12,60 @@ var app = {
     },
     // deviceready Event Handler
     //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        console.log("We got to device ready");
+        app.receivedEvent('deviceready');
+
+        // demo the scan
+        console.log('about to scan');
+        try {
+            var scanned = app.scan();
+            console.log('scan triggered', scanned);
+        } catch (e) {
+            console.log('scan failed');
+            console.log(JSON.stringify(e));
+            console.log('that sucks... reloading in 10');
+            setTimeout(function() {
+                console.log('reloading now...');
+                app.onDeviceReady();
+            }, 10000);
+        }
     },
-    // Scan a barcode
-    //
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+    /**
+* here is an example for scanning a barcode...
+* obviously, your own JS logic <<here>>
+*
+* Note the require() method is called on window.cordova
+* this is different than the readme!
+*/
     scan: function() {
-        scaner.scan( function(result) {
-            alert("We got a barcode\n" +
-                  "Result: " + result.text + "\n" +
-                  "Format: " + result.format + "\n" +
-                  "Cancelled: " + result.cancelled);
-        }, function(error) {
-            alert("Scanning failed: " + error);
-        });
-    },
-    // Encode text into QR code
-    //
-    encode: function() {
-        scaner.encode(BarcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
-            alert("encode success: " + success);
-        }, function(fail) {
-            alert("encoding failed: " + fail);
-        }); 
+        console.log('scan(): init');
+        // documentation said the syntax was this:
+        // var scanner = window.PhoneGap.require("cordova/plugin/BarcodeScanner");
+        // but playing with options, seems like it should be this:
+        var scanner = window.cordova.require("cordova/plugin/BarcodeScanner");
+        scanner.scan(
+                function (result) {
+                    alert("We got a barcode\n" +
+                        "Result: " + result.text + "\n" +
+                        "Format: " + result.format + "\n" +
+                        "Cancelled: " + result.cancelled);
+                },
+                function (error) {
+                    alert("Scanning failed: " + error);
+                }
+                );
     }
 };
