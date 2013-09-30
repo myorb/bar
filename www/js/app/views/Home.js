b26ec31ed2ec6d85f0c372a414870aa4d7082aac
alex <itmedved@gmail.com>
2013-09-30 12:28:55 +0300

    qr code reader

---
+++ www/js/app/views/Home.js
@@ -0,0 +1,83 @@
+define(function (require) {
+
+    "use strict";
+
+    var $                   = require('jquery'),
+        _                   = require('underscore'),
+        Backbone            = require('backbone'),
+        EmployeeListView    = require('app/views/EmployeeList'),
+        models              = require('app/models/employee'),
+        tpl                 = require('text!tpl/Home.html'),
+
+        template = _.template(tpl);
+
+
+    return Backbone.View.extend({
+
+        initialize: function () {
+            this.employeeList = new models.EmployeeCollection();
+            this.render();
+        },
+
+        render: function () {
+            this.$el.html(template());
+            this.listView = new EmployeeListView({collection: this.employeeList, el: $(".scroller", this.el)});
+            return this;
+        },
+
+        events: {
+            "keyup .search-key":    "search",
+            "keypress .search-key": "onkeypress",
+            "click .search_form": "show_search_form",
+            "click .slot_mashine": "show_slot_mashine",
+            "click .go_home": "go_home",
+            "click .com_in_bar": "com_in_bar",
+        },
+
+        com_in_bar: function(event){
+             var parentElement = event;
+        // documentation said the syntax was this:
+        // var scanner = window.PhoneGap.require("cordova/plugin/BarcodeScanner");
+        // but playing with options, seems like it should be this:
+        //var scanner = window.cordova.require("cordova/plugin/BarcodeScanner");
+        barcodeScanner.scan(
+                function (result) {
+                    alert("We got a barcode\n" +
+                        "Result: " + result.text + "\n" +
+                        "Format: " + result.format + "\n" +
+                        "Cancelled: " + result.cancelled);
+                },
+                function (error) {
+                    alert("Scanning failed: " + error);
+                }
+                );
+            
+        },
+
+        show_search_form: function(event){
+            $(".search-bar").toggle();
+        },
+
+        show_slot_mashine:function(event){
+            $(".topcoat-tab-bar__item, .topcoat-button-bar").hide();
+            $('.go_home').show();
+        },
+        go_home:function(event){
+            $(".topcoat-tab-bar__item, .topcoat-button-bar").show();
+            $('.go_home').hide();
+        },
+
+        search: function (event) {
+            var key = $('.search-key').val();
+            this.employeeList.fetch({reset: true, data: {name: key}});
+        },
+
+        onkeypress: function (event) {
+            if (event.keyCode === 13) { // enter key pressed
+                event.preventDefault();
+            }
+        }
+
+    });
+
+});
---
+++ www/js/app/views/Paige.js
@@ -0,0 +1,51 @@
+define(function (require) {
+
+    "use strict";
+
+    var $                   = require('jquery'),
+        _                   = require('underscore'),
+        Backbone            = require('backbone'),
+        EmployeeListView    = require('app/views/EmployeeList'),
+        models              = require('app/models/employee'),
+        tpl                 = require('text!tpl/Pome.html'),
+
+        template = _.template(tpl);
+
+
+    return Backbone.View.extend({
+
+        initialize: function () {
+            this.employeeList = new models.EmployeeCollection();
+            this.render();
+        },
+
+        render: function () {
+            this.$el.html(template());
+            this.listView = new EmployeeListView({collection: this.employeeList, el: $(".scroller", this.el)});
+            return this;
+        },
+
+        events: {
+            "keyup .search-key":    "search",
+            "keypress .search-key": "onkeypress",
+            "click .search_paige": "show_search_form",
+        },
+
+        show_search_form: function(){
+            $(".search-bar").slideToggle( "slow" );
+        },
+
+        search: function (event) {
+            var key = $('.search-key').val();
+            this.employeeList.fetch({reset: true, data: {name: key}});
+        },
+
+        onkeypress: function (event) {
+            if (event.keyCode === 13) { // enter key pressed
+                event.preventDefault();
+            }
+        }
+
+    });
+
+});
